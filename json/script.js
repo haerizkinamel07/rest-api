544b8eaaeccb58116a9668f1cc627730cff577e6
haerizkinamel07 <ahaerizkina@gmail.com>
2019-12-06 20:43:39 +0700

    menambahkan project json

---
+++ json/script.js
@@ -0,0 +1,24 @@
+// let mahasiswa = {
+//     nama: " Amelia Haerizkina",
+//     nrp: "173040083",
+//     email: "173040083a@mail.unpas.ac.id"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
+//jalankan ajax
+// xhr.open('GET', 'coba.json', true);
+// xhr.send();
+
+//jquery
+
+$.getJSON('coba.json', function (data) {
+    console.log(data);
+});
